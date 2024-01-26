c292df3e50ac2edf10b76ca8be12df1ebf12b7c4
Sebastiaan Speck <shem.speck@gmail.com>
2024-01-26 17:12:02 +0100

    Use case-insensitive keywords

--- grammar.js
+++ grammar.js
@@ -35,7 +35,7 @@ module.exports = grammar({
       prec.left(seq(repeat1(seq($._top_level_statement, $._line_terminator)), optional($._dollar))),
 
     package: $ =>
-      prec.left(seq('_package', $._identifier, repeat($.fragment))),
+      prec.left(seq(alias(/_package/i, '_package'), $._identifier, repeat($.fragment))),
 
     _dollar: $ => token(seq('$', optional('\r'), '\n')),
 
@@ -51,10 +51,10 @@ module.exports = grammar({
     method: $ =>
       prec.left(
         seq(
-          optional('_abstract'),
-          optional('_private'),
-          optional('_iter'),
-          '_method',
+          optional(alias(/_abstract/i, '_abstract')),
+          optional(alias(/_private/i, '_private')),
+          optional(alias(/_iter/i, '_iter')),
+          alias(/_method/i, '_method'),
           field('exemplarname', $.identifier),
           choice(
             seq(
@@ -62,9 +62,9 @@ module.exports = grammar({
               optional(choice(
                 seq(
                   '(',
-                  optional($._arguments),
-                  optional(seq('_optional', $._arguments)),
-                  optional(seq('_gather', $.argument)),
+                  optional(seq($.argument, repeat(seq(',', $.argument)))),
+                  optional(seq(optional(','), alias(/_optional/i, '_optional'), $._arguments)),
+                  optional(seq(optional(','), alias(/_gather/i, '_gather'), $.argument)),
                   ')', optional(seq(choice('<<', '^<<'), $._arguments))),
                 seq('[', optional($._arguments), ']', optional(seq(choice('<<', '^<<'), $._arguments))),
                 seq(choice('<<', '^<<'), $._arguments),
@@ -73,7 +73,7 @@ module.exports = grammar({
           $._line_terminator,
           optional($.documentation),
           optional($._codeblock),
-          '_endmethod',
+          alias(/_endmethod/i, '_endmethod'),
         ),
       ),
 
@@ -81,17 +81,17 @@ module.exports = grammar({
     // <block body>
     // _endproc
     procedure: $ =>
-      seq(optional('_iter'), '_proc',
+      seq(optional(alias(/_iter/i, '_iter')), alias(/_proc/i, '_proc'),
         optional($.label),
         seq(
           '(',
-          optional($._arguments),
-          optional(seq('_optional', $._arguments)),
-          optional(seq('_gather', $.argument)),
+          optional(seq($.argument, repeat(seq(',', $.argument)))),
+          optional(seq(optional(','), alias(/_optional/i, '_optional'), $._arguments)),
+          optional(seq(optional(','), alias(/_gather/i, '_gather'), $.argument)),
           ')', optional(seq(choice('<<', '^<<'), $._arguments)),
         ),
         optional($._codeblock),
-        '_endproc',
+        alias(/_endproc/i, '_endproc'),
       ),
 
     argument: $ => $._identifier,
@@ -104,7 +104,7 @@ module.exports = grammar({
     // _endblock
     block: $ =>
       prec.left(
-        seq('_block', optional($.label), optional($._codeblock), '_endblock'),
+        seq(alias(/_block/i, '_block'), optional($.label), optional($._codeblock), alias(/_endblock/i, '_endblock')),
       ),
 
     assignment: $ =>
@@ -125,51 +125,51 @@ module.exports = grammar({
     //   <block body> ]
     // _endif
     if: $ =>
-      seq('_if',
+      seq(alias(/_if/i, '_if'),
         field('condition', $._expression),
-        '_then',
+        alias(/_then/i, '_then'),
         optional($._codeblock),
         repeat($.elif),
         optional($.else),
-        '_endif',
+        alias(/_endif/i, '_endif'),
       ),
 
-    elif: $ => seq('_elif', field('condition', $._expression), '_then', optional($._codeblock)),
+    elif: $ => seq(alias(/_elif/i, '_elif'), field('condition', $._expression), alias(/_then/i, '_then'), optional($._codeblock)),
 
-    else: $ => seq('_else', optional($._codeblock)),
+    else: $ => seq(alias(/_else/i, '_else'), optional($._codeblock)),
 
     // _loop [ @ <identifier> ]
     //  <block body>
     // _endloop
     loop: $ =>
       seq(
-        '_loop',
+        alias(/_loop/i, '_loop'),
         optional($.label),
         optional($._codeblock),
-        '_endloop',
+        alias(/_endloop/i, '_endloop'),
       ),
 
     // [ _finally [ _with <lvalue tuple> ]
     //  <block body> ]
-    finally: $ => seq('_finally', optional(seq('_with', $._identifier_list)), optional($._codeblock)),
+    finally: $ => seq(alias(/_finally/i, '_finally'), optional(seq(alias(/_with/i, '_with'), $._identifier_list)), optional($._codeblock)),
 
     // _handling condition _with procedure
     handling: $ =>
-      prec.left(seq('_handling', choice(
-        '_default',
-        seq(field('condition', $._expression), repeat(seq(',', field('condition', $._expression))), '_with', choice('_default', $._expression))),
+      prec.left(seq(alias(/_handling/i, '_handling'), choice(
+        alias(/_default/i, '_default'),
+        seq(field('condition', $._expression), repeat(seq(',', field('condition', $._expression))), alias(/_with/i, '_with'), choice(alias(/_default/i, '_default'), $._expression))),
       )),
 
     // _catch <expression>
     //  <block body>
     // _endcatch
-    catch: $ => seq('_catch', $._expression, $._terminator, optional($._codeblock), '_endcatch'),
+    catch: $ => seq(alias(/_catch/i, '_catch'), $._expression, $._terminator, optional($._codeblock), alias(/_endcatch/i, '_endcatch')),
 
     // _throw <expression> [ _with <rvalue tuple> ]
-    throw: $ => prec.left(seq('_throw', $._expression, optional(seq('_with', $._expression_list)))),
+    throw: $ => prec.left(seq(alias(/_throw/i, '_throw'), $._expression, optional(seq(alias(/_with/i, '_with'), $._expression_list)))),
 
     // _primitive <number>
-    primitive: $ => seq('_primitive', $.number),
+    primitive: $ => seq(alias(/_primitive/i, '_primitive'), $.number),
 
     // [ _for <lvalue tuple> ] _over <iter invocation>
     // _loop [ @<identifier> ]
@@ -179,14 +179,14 @@ module.exports = grammar({
     // _endloop
     iterator: $ =>
       seq(
-        optional(seq('_for', $._identifier_list)),
-        '_over', $._expression,
+        optional(seq(alias(/_for/i, '_for'), $._identifier_list)),
+        alias(/_over/i, '_over'), $._expression,
         seq(
-          '_loop',
+          alias(/_loop/i, '_loop'),
           optional($.label),
           optional($._codeblock),
           optional($.finally),
-          '_endloop'),
+          alias(/_endloop/i, '_endloop')),
       ),
 
     // _while <condition>
@@ -194,12 +194,12 @@ module.exports = grammar({
     //  <block body>
     // _endloop
     while: $ =>
-      seq('_while', field('condition', $._expression),
+      seq(alias(/_while/i, '_while'), field('condition', $._expression),
         seq(
-          '_loop',
+          alias(/_loop/i, '_loop'),
           optional($.label),
           optional($._codeblock),
-          '_endloop'),
+          alias(/_endloop/i, '_endloop')),
       ),
 
     // _try [ _with <name list> ]
@@ -209,27 +209,27 @@ module.exports = grammar({
     // _endtry
     try: $ =>
       seq(
-        '_try',
-        optional(seq('_with', field('condition', $.identifier))),
+        alias(/_try/i, '_try'),
+        optional(seq(alias(/_with/i, '_with'), field('condition', $.identifier))),
         optional($._codeblock),
-        repeat(seq('_when',
+        repeat(seq(alias(/_when/i, '_when'),
           field('raised_condition', $.identifier), repeat(seq(',', field('raised_condition', $.identifier))),
           optional($._codeblock))),
-        '_endtry',
+        alias(/_endtry/i, '_endtry'),
       ),
 
     loopbody: $ =>
       seq(
-        '_loopbody',
+        alias(/_loopbody/i, '_loopbody'),
         '(', seq($._expression, repeat(seq(',', $._expression))), ')',
       ),
 
     // _leave [ @ <identifier> ] [_with <rvalue tuple> ]
     leave: $ =>
       prec.left(seq(
-        '_leave',
+        alias(/_leave/i, '_leave'),
         optional($.label),
-        optional(seq('_with', choice(
+        optional(seq(alias(/_with/i, '_with'), choice(
           seq('(', seq($._expression, repeat1(seq(',', $._expression))), ')'),
           seq($._expression, repeat(seq(',', $._expression)))))),
       )),
@@ -237,9 +237,9 @@ module.exports = grammar({
     // _continue _with <rvalue tuple>
     continue: $ =>
       prec.left(seq(
-        '_continue',
+        alias(/_continue/i, '_continue'),
         optional($.label),
-        optional(seq('_with', choice(
+        optional(seq(alias(/_with/i, '_with'), choice(
           seq('(', seq($._expression, repeat1(seq(',', $._expression))), ')'),
           seq($._expression, repeat(seq(',', $._expression)))))),
       )),
@@ -251,12 +251,12 @@ module.exports = grammar({
     // _endprotect
     protect: $ =>
       seq(
-        '_protect',
-        optional(seq('_locking', $._expression, $._terminator)),
+        alias(/_protect/i, '_protect'),
+        optional(seq(alias(/_locking/i, '_locking'), $._expression, $._terminator)),
         optional($._codeblock),
-        '_protection',
+        alias(/_protection/i, '_protection'),
         optional($._codeblock),
-        '_endprotect',
+        alias(/_endprotect/i, '_endprotect'),
       ),
 
     // _lock <expression>
@@ -264,14 +264,14 @@ module.exports = grammar({
     // _endlock
     lock: $ =>
       seq(
-        '_lock',
+        alias(/_lock/i, '_lock'),
         seq($._expression, $._terminator),
         optional($._codeblock),
-        '_endlock',
+        alias(/_endlock/i, '_endlock'),
       ),
 
     // _pragma (classify_level=<level>, topic={<set of topics>}, [ usage={<set of usages>} ] )
-    pragma: $ => prec.left(seq('_pragma', /(.*)/)),
+    pragma: $ => prec.left(seq(alias(/_pragma/i, '_pragma'), /(.*)/)),
 
     _literal: $ =>
       choice(
@@ -327,17 +327,17 @@ module.exports = grammar({
     _expression_list: $ =>
       prec.right(seq($._expression, repeat(seq(',', $._expression)))),
 
-    true: $ => '_true',
-    false: $ => '_false',
-    maybe: $ => '_maybe',
-    unset: $ => '_unset',
-    super: $ => '_super',
-    self: $ => '_self',
-    clone: $ => '_clone',
+    true: $ => alias(/_true/i, '_true'),
+    false: $ => alias(/_false/i, '_false'),
+    maybe: $ => alias(/_maybe/i, '_maybe'),
+    unset: $ => alias(/_unset/i, '_unset'),
+    super: $ => alias(/_super/i, '_super'),
+    self: $ => alias(/_self/i, '_self'),
+    clone: $ => alias(/_clone/i, '_clone'),
 
-    thisthread: $ => '_thisthread',
+    thisthread: $ => alias(/_thisthread/i, '_thisthread'),
 
-    class: $ => seq('_class', field('java_classname', seq(/\|[a-zA-Z\d\.]*\|/))),
+    class: $ => seq(alias(/_class/i, '_class'), field('java_classname', seq(/\|[a-zA-Z\d\.]*\|/))),
 
     _terminator: $ =>
       choice(';', $._line_terminator),
@@ -400,43 +400,43 @@ module.exports = grammar({
       $.global,
       $.import),
 
-    global: $ => seq('_global', choice($.identifier, $.global_variable, $.dynamic_variable), repeat(seq(',', choice($.identifier, $.global_variable, $.dynamic_variable)))),
+    global: $ => seq(alias(/_global/i, '_global'), choice($.identifier, $.global_variable, $.dynamic_variable), repeat(seq(',', choice($.identifier, $.global_variable, $.dynamic_variable)))),
 
     local: $ => prec.left(
-      seq('_local',
+      seq(alias(/_local/i, '_local'),
         choice(
           seq('(', seq($.identifier, optional(seq('<<', $._expression))), repeat(seq(',', seq($.identifier, optional(seq('<<', $._expression))))), ')'),
-          seq('(', seq($.identifier, optional(seq('<<', $._expression))), repeat(seq(',', seq($.identifier, optional(seq('<<', $._expression))))), seq(',', '_gather', seq($.identifier, optional(seq('<<', $._expression)))), ')'),
-          seq('(', seq('_gather', $.identifier, optional(seq('<<', $._expression))), ')'),
+          seq('(', seq($.identifier, optional(seq('<<', $._expression))), repeat(seq(',', seq($.identifier, optional(seq('<<', $._expression))))), seq(',', alias(/_gather/i, '_gather'), seq($.identifier, optional(seq('<<', $._expression)))), ')'),
+          seq('(', seq(alias(/_gather/i, '_gather'), $.identifier, optional(seq('<<', $._expression))), ')'),
           seq(seq($.identifier, optional(seq('<<', $._expression))), repeat(seq(',', seq($.identifier, optional(seq('<<', $._expression))))))),
         optional(seq('<<', $._expression)))),
 
     _global_assignment: $ =>
       seq(
         optional($.pragma),
-        '_global', optional('_constant'), choice($.identifier, $.dynamic_variable), '<<', $._expression),
+        alias(/_global/i, '_global'), optional(alias(/_constant/i, '_constant')), choice($.identifier, $.dynamic_variable), '<<', $._expression),
 
     constant: $ =>
       seq(
         choice(
-          '_constant',
-          seq('_constant', '_local'),
-          seq('_local', '_constant')),
+          alias(/_constant/i, '_constant'),
+          seq(alias(/_constant/i, '_constant'), alias(/_local/i, '_local')),
+          seq(alias(/_local/i, '_local'), alias(/_constant/i, '_constant'))),
         choice(
           seq('(', $._identifier_list, ')'),
           $._identifier_list),
         seq('<<', $._expression)),
 
-    dynamic: $ => seq('_dynamic', $.dynamic_variable, repeat(seq(',', $.dynamic_variable)), optional(seq('<<', $._expression))),
+    dynamic: $ => seq(alias(/_dynamic/i, '_dynamic'), $.dynamic_variable, repeat(seq(',', $.dynamic_variable)), optional(seq('<<', $._expression))),
 
-    import: $ => seq('_import', $._identifier_list),
+    import: $ => seq(alias(/_import/i, '_import'), $._identifier_list),
 
-    dynamic_import: $ => seq('_dynamic', '_import', $.dynamic_variable, repeat(seq(',', $.dynamic_variable))),
+    dynamic_import: $ => seq(alias(/_dynamic/i, '_dynamic'), alias(/_import/i, '_import'), $.dynamic_variable, repeat(seq(',', $.dynamic_variable))),
 
     return: $ =>
       prec.right(
         choice(
-          seq('_return', optional($._expression_list)),
+          seq(alias(/_return/i, '_return'), optional($._expression_list)),
           seq('>>', $._expression_list),
         ),
       ),
@@ -449,9 +449,9 @@ module.exports = grammar({
           $.call)),
       ),
 
-    gather: $ => seq('_gather', $._expression),
-    scatter: $ => seq('_scatter', $._expression),
-    allresults: $ => seq('_allresults', $._expression),
+    gather: $ => seq(alias(/_gather/i, '_gather'), $._expression),
+    scatter: $ => seq(alias(/_scatter/i, '_scatter'), $._expression),
+    allresults: $ => seq(alias(/_allresults/i, '_allresults'), $._expression),
 
     parenthesized_expression: $ => seq('(', $._expression_list, ')'),
 
@@ -500,7 +500,7 @@ module.exports = grammar({
       prec.right(PREC.RELATIONAL,
         seq(
           field('left', $._expression),
-          field('operator', choice('_is', '_isnt', '_cf', '=', '~=', '<>', '>=', '<=', '<', '>')),
+          field('operator', choice(alias(/_is/i, '_is'), alias(/_isnt/i, '_isnt'), alias(/_cf/i, '_cf'), '=', '~=', '<>', '>=', '<=', '<', '>')),
           field('right', $._expression),
         ),
       ),
@@ -509,7 +509,7 @@ module.exports = grammar({
       prec.left(PREC.BOOLEAN,
         seq(
           field('left', $._expression),
-          field('operator', choice('_and', '_or', '_xor', '_andif', '_orif', '_xorif')),
+          field('operator', choice(alias(/_and/i, '_and'), alias(/_or/i, '_or'), alias(/_xor/i, '_xor'), alias(/_andif/i, '_andif'), alias(/_orif/i, '_orif'), alias(/_xorif/i, '_xorif'))),
           field('right', $._expression),
         ),
       ),
@@ -518,13 +518,13 @@ module.exports = grammar({
       prec.left(PREC.ARITHMETIC,
         seq(
           field('left', $._expression),
-          field('operator', choice('**', '*', '/', '_mod', '_div', '-', '+')),
+          field('operator', choice('**', '*', '/', alias(/_mod/i, '_mod'), alias(/_div/i, '_div'), '-', '+')),
           field('right', $._expression),
         ),
       ),
 
     unary_operator: $ =>
-      prec.right(seq(field('operator', choice('+', '-', '_not', '~')), $._expression)),
+      prec.right(seq(field('operator', choice('+', '-', alias(/_not/i, '_not'), '~')), $._expression)),
 
     symbol: $ => /:(\|[^|]*\||[a-zA-Z0-9_\?!]+)+/,
 
