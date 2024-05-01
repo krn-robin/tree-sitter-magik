af498fc516da38349b605faadf1df0897986fae1
Sebastiaan Speck <12570668+sebastiaanspeck@users.noreply.github.com>
2024-05-01 09:00:17 +0200

    Migrate from eslintrc format to new flat config format

--- eslint.config.js
+++ eslint.config.js
@@ -1,21 +1,20 @@
-module.exports = {
-  'env': {
-    'commonjs': true,
-    'es2021': true,
-  },
-  'extends': 'google',
-  'overrides': [
-  ],
-  'parserOptions': {
-    'ecmaVersion': 'latest',
-    'sourceType': 'module',
-  },
-  'rules': {
-    'arrow-parens': "off",
-    'indent': ['error', 2, {'SwitchCase': 1}],
-    'max-len': [
-      'error',
-      {'code': 120, 'ignoreComments': true, 'ignoreUrls': true, 'ignoreStrings': true},
+import globals from "globals";
+
+export default [
+  {
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      globals: {
+        ...globals.commonjs,
+        ...globals.es2021,
+      }
+      rules: {
+        "arrow-parens": "off",
+        "indent": ["error", 2, {"SwitchCase": 1}],
+        "max-len": [
+          "error",
+          {"code": 120, "ignoreComments": true, "ignoreUrls": true, "ignoreStrings": true},
     ],
   },
 };
