c70fdf3db1f8ff6bd981f6b1a6d58ee8da9e0680
Sebastiaan Speck <12570668+sebastiaanspeck@users.noreply.github.com>
2024-05-01 11:55:09 +0200

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,12 @@
 module.exports [
-  {
-    languageOptions: {
-      rules: {
-        "arrow-parens": "off",
-        "indent": ["error", 2, {"SwitchCase": 1}],
-        "max-len": [
-          "error",
-          {"code": 120, "ignoreComments": true, "ignoreUrls": true, "ignoreStrings": true},
-    ],
-  },
-};
+    {
+        rules: {
+            "arrow-parens": "off",
+            "indent": ["error", 2, {"SwitchCase": 1}],
+            "max-len": [
+                "error",
+                {"code": 120, "ignoreComments": true, "ignoreUrls": true, "ignoreStrings": true},
+            ],
+        }
+    }
+];
