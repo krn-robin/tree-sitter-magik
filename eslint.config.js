d671e706778ba9ea229f0aaede156231a3445e2c
Sebastiaan Speck <12570668+sebastiaanspeck@users.noreply.github.com>
2024-05-01 20:17:10 +0200

    Rename .eslintrc.js to eslint.config.js and remove eslint-config-google (#46)

---
+++ eslint.config.js
@@ -0,0 +1,12 @@
+module.exports [
+    {
+        rules: {
+            "arrow-parens": "off",
+            "indent": ["error", 2, {"SwitchCase": 1}],
+            "max-len": [
+                "error",
+                {"code": 120, "ignoreComments": true, "ignoreUrls": true, "ignoreStrings": true},
+            ],
+        }
+    }
+];
