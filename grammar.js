4fa2d56ed98ae0b4e503e7688610c50ee8e7ca2d
Sebastiaan Speck <shem.speck@gmail.com>
2025-07-08 09:39:46 +0200

    Fix the errors

--- grammar.js
+++ grammar.js
@@ -53,8 +53,7 @@ module.exports = grammar({
 
     _method_declaration: $ =>
       seq(
-        optional($.pragma),
-        optional($.documentation),
+        optional(seq($.pragma, optional($.documentation))),
         $.method,
       ),
 
@@ -433,8 +432,7 @@ module.exports = grammar({
 
     _global_assignment: $ =>
       seq(
-        optional($.pragma),
-        optional($.documentation),
+        optional(seq($.pragma, optional($.documentation))),
         alias(/_global/i, '_global'), optional(alias(/_constant/i, '_constant')), choice($.identifier, $.dynamic_variable), '<<', $._expression),
 
     constant: $ =>
@@ -557,6 +555,7 @@ module.exports = grammar({
     symbol: $ => /:(\|[^|]*\||[\p{L}\p{N}_?!]+)+/u,
 
     documentation: $ => prec.right(repeat1(/##.*/)),
+
     comment: $ => token(prec(PREC.COMMENT, /#.*/)),
   },
 });
